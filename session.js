ca1a1845c5e5a265ddd9876b9beefa7f01313c60
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-02 18:32:44 +0800

    new

--- session.js
+++ session.js
@@ -1,16 +1,24 @@
 const keys = require("./config_keys/keys");
-const redis = require("redis");
 
 class Session {
   constructor() {
-    const client = redis.createClient(keys.redisPort);
+    if (process.env.REDISTOGO_URL) {
+      var rtg = require("url").parse(process.env.REDISTOGO_URL);
+      var redis = require("redis").createClient(rtg.port, rtg.hostname);
+      var rtg = require("url").parse(process.env.REDISTOGO_URL);
+      var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    } else {
+      const redis = require("redis").createClient(keys.redisPort);
+    }
     console.log("Client creating!");
   }
 
   setAdminList() {
     const adminsId = keys.adminsId;
-    return client.setex(adminsId, 3600, adminsId);
+    return redis.setex(adminsId, 3600, adminsId);
   }
 }
 
 module.exports = Session;
+
+//redis://redistogo:8ea37dd6493bf8b88c4f18ed0247240a@hammerjaw.redistogo.com:10191/
