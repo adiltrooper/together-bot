27a37557db34047ff4e6928ead416debfed7299a
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-02 19:15:48 +0800

    new

--- session.js
+++ session.js
@@ -6,7 +6,7 @@ class Session {
     if (process.env.REDISTOGO_URL) {
       console.log("YES");
       var rtg = require("url").parse(process.env.REDISTOGO_URL);
-      var redis = this.redis.createClient(rtg.port, rtg.hostname);
+      var redis = redis.createClient(rtg.port, rtg.hostname);
 
       redis.auth(rtg.auth.split(":")[1]);
     } else {
