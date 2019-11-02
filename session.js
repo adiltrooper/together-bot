d45f8e062882540adf2b769b7c000642723d5f2c
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-02 13:13:28 +0800

    new

--- session.js
+++ session.js
@@ -1,10 +1,11 @@
-const keys = require("./config_keys/keys");
-const redis = require("redis");
-
-class Session {
-  constructor() {
-    const client = redis.createClient(keys.redisPort);
-  }
-}
-
-module.exports = Session;
+// const keys = require("./config_keys/keys");
+// const redis = require("redis");
+//
+//
+// class Session {
+//   constructor() {
+//     const client = redis.createClient(keys.redisPort);
+//   }
+// }
+//
+// module.exports = Session;
