d17eb4f15f4681da029ec529ff6ba2ceaa0f729b
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-02 12:26:37 +0800

    correction

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const axios = require("axios");
 const mysql = require("mysql");
 const db = require("./config_db/db");
 const express = require("express");
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 
 const connection = mysql.createConnection(db);
 
