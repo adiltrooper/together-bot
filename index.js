429a71febfa732a5ad723d81ec7b0d9274b2585e
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-02 13:16:34 +0800

    new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const TelegramBot = require("node-telegram-bot-api"),
   });
 bot.setWebHook(externalUrl + `:${process.env.PORT}/bot` + token);
 
-const Session = require("./session");
+// const Session = require("./session");
 
 const keys = require("./config_keys/keys");
 const axios = require("axios");
