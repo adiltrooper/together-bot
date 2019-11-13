62c95064531843246aa7916943d41cf868b02438
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-11-13 21:56:39 +0800

    extract chat_id from db

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ bot.on("message", async msg => {
         .slice(1, userSendList.length - 1)
         .split(",")
         .map(numberString => {
-          parseInt(numberString);
+          return Number(numberString);
         });
       console.log(userSendList);
     };
