de871c9bd6d77cfd805dffd9d074632e478d4da7
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-02 17:08:45 +0800

    caching list of activities

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ ${short_desc}
         switch (msg.text) {
           case "Feelin' Adventurous":
             return connection.query(
-              "SELECT location, activity, short_desc, price, poi, website, bot_category.category_name AS category, imageURL FROM bot_listings_db LEFT JOIN bot_listing_category ON bot_listings_db.id = bot_listing_id LEFT JOIN bot_category ON bot_category_id = bot_category.id WHERE bot_category_id = 1 ORDER BY RAND() LIMIT 2",
+              "SELECT location, activity, short_desc, price, poi, website, bot_category.category_name AS category, imageURL FROM bot_listings_db LEFT JOIN bot_listing_category ON bot_listings_db.id = bot_listing_id LEFT JOIN bot_category ON bot_category_id = bot_category.id WHERE bot_category_id = 1 ORDER BY RAND() LIMIT 10",
               function(err, results, fields) {
                 if (err) {
                   console.log(err.message);
