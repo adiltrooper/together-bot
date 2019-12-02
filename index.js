fc15333fb0155f85ccbe1dba893f39be3e6a41d7
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-02 20:48:13 +0800

    caching list of activities

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ ${short_desc}
             "SELECT location, activity, short_desc, price, poi, website, bot_category.category_name AS category, imageURL FROM bot_listings_db LEFT JOIN bot_listing_category ON bot_listings_db.id = bot_listing_id LEFT JOIN bot_category ON bot_category_id = bot_category.id WHERE bot_category_id = ? ORDER BY RAND() LIMIT 1",
             [cat_id],
             function(err, results, fields) {
-              bot.sendPhoto(119860989, imageURL, {
+              bot.sendPhoto(119860989, results.imageURL, {
                 caption: `<b>☀️${results.activity} @ ${results.location}☀️</b>
 
       ${results.short_desc}
