c1b27f069e87797362009b9f416b1e51e815b8d7
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-24 20:08:13 +0800

    seperate out option functions

--- customMessageFn.js
+++ customMessageFn.js
@@ -1,93 +1,91 @@
-module.exports = {
-  function(option1, option2, option3, option4) {
-    if (option1 && !option2 && !option3 && !option4) {
-      bot.sendMessage(userId, draftCustomMessage, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: option1[1],
-                callback_data: option1[1]
-              }
-            ]
+module.exports = function(option1, option2, option3, option4) {
+  if (option1 && !option2 && !option3 && !option4) {
+    bot.sendMessage(userId, draftCustomMessage, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: option1[1],
+              callback_data: option1[1]
+            }
           ]
-        }
-      });
-    } else if (option1 && option2 && !option3 && !option4) {
-      bot.sendMessage(userId, draftCustomMessage, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: option1[1],
-                callback_data: option1[1]
-              }
-            ],
-            [
-              {
-                text: option2[1],
-                callback_data: option2[1]
-              }
-            ]
+        ]
+      }
+    });
+  } else if (option1 && option2 && !option3 && !option4) {
+    bot.sendMessage(userId, draftCustomMessage, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: option1[1],
+              callback_data: option1[1]
+            }
+          ],
+          [
+            {
+              text: option2[1],
+              callback_data: option2[1]
+            }
           ]
-        }
-      });
-    } else if (option1 && option2 && option3 && !option4) {
-      bot.sendMessage(userId, draftCustomMessage, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: option1[1],
-                callback_data: option1[1]
-              }
-            ],
-            [
-              {
-                text: option2[1],
-                callback_data: option2[1]
-              }
-            ],
-            [
-              {
-                text: option3[1],
-                callback_data: option3[1]
-              }
-            ]
+        ]
+      }
+    });
+  } else if (option1 && option2 && option3 && !option4) {
+    bot.sendMessage(userId, draftCustomMessage, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: option1[1],
+              callback_data: option1[1]
+            }
+          ],
+          [
+            {
+              text: option2[1],
+              callback_data: option2[1]
+            }
+          ],
+          [
+            {
+              text: option3[1],
+              callback_data: option3[1]
+            }
           ]
-        }
-      });
-    } else if (option1 && option2 && option3 && option4) {
-      bot.sendMessage(userId, draftCustomMessage, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: option1[1],
-                callback_data: option1[1]
-              }
-            ],
-            [
-              {
-                text: option2[1],
-                callback_data: option2[1]
-              }
-            ],
-            [
-              {
-                text: option3[1],
-                callback_data: option3[1]
-              }
-            ],
-            [
-              {
-                text: option4[1],
-                callback_data: option4[1]
-              }
-            ]
+        ]
+      }
+    });
+  } else if (option1 && option2 && option3 && option4) {
+    bot.sendMessage(userId, draftCustomMessage, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: option1[1],
+              callback_data: option1[1]
+            }
+          ],
+          [
+            {
+              text: option2[1],
+              callback_data: option2[1]
+            }
+          ],
+          [
+            {
+              text: option3[1],
+              callback_data: option3[1]
+            }
+          ],
+          [
+            {
+              text: option4[1],
+              callback_data: option4[1]
+            }
           ]
-        }
-      });
-    }
+        ]
+      }
+    });
   }
 };
