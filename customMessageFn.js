9ae6189d816af9ab311127ea06a6ac4be27d2fd5
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-24 20:26:28 +0800

    seperate out option functions

--- customMessageFn.js
+++ customMessageFn.js
@@ -5,8 +5,8 @@ module.exports = function(option1, option2, option3, option4) {
         inline_keyboard: [
           [
             {
-              text: option1[1],
-              callback_data: option1[1]
+              text: option1,
+              callback_data: option1
             }
           ]
         ]
@@ -18,14 +18,14 @@ module.exports = function(option1, option2, option3, option4) {
         inline_keyboard: [
           [
             {
-              text: option1[1],
-              callback_data: option1[1]
+              text: option1,
+              callback_data: option1
             }
           ],
           [
             {
-              text: option2[1],
-              callback_data: option2[1]
+              text: option2,
+              callback_data: option2
             }
           ]
         ]
@@ -37,20 +37,20 @@ module.exports = function(option1, option2, option3, option4) {
         inline_keyboard: [
           [
             {
-              text: option1[1],
-              callback_data: option1[1]
+              text: option1,
+              callback_data: option1
             }
           ],
           [
             {
-              text: option2[1],
-              callback_data: option2[1]
+              text: option2,
+              callback_data: option2
             }
           ],
           [
             {
-              text: option3[1],
-              callback_data: option3[1]
+              text: option3,
+              callback_data: option3
             }
           ]
         ]
@@ -62,26 +62,26 @@ module.exports = function(option1, option2, option3, option4) {
         inline_keyboard: [
           [
             {
-              text: option1[1],
-              callback_data: option1[1]
+              text: option1,
+              callback_data: option1
             }
           ],
           [
             {
-              text: option2[1],
-              callback_data: option2[1]
+              text: option2,
+              callback_data: option2
             }
           ],
           [
             {
-              text: option3[1],
-              callback_data: option3[1]
+              text: option3,
+              callback_data: option3
             }
           ],
           [
             {
-              text: option4[1],
-              callback_data: option4[1]
+              text: option4,
+              callback_data: option4
             }
           ]
         ]
