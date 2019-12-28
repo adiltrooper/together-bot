1bf1ddfc44ce10cbaf864180dffc5c843d76af04
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-28 13:31:17 +0800

    custom post options

--- session.js
+++ session.js
@@ -131,7 +131,7 @@ class Session {
   }
 
   setPollMessage(message) {
-    redis.hset("currentPoll", "message", message, function(err, res) {
+    redis.hset("poll:currentPoll", "message", message, function(err, res) {
       if (err) {
         console.log(err);
       } else {
