e534e9aaf251b9f347651cb3852a0409d88e6071
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-29 20:58:40 +0800

    custom post options

--- index.js
+++ index.js
@@ -1004,7 +1004,7 @@ bot.onText(/Send Post/, async msg => {
       if (err) console.log(err);
     });
   }
-  getUsers();
+  const getUserFn = await getUsers();
   if (adminState == "admin3") {
     const retrieveUserList = async () => {
       var userSendList = await session.getUserSendList().catch(err => {
