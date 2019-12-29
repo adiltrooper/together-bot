6eb655b05a00f48caece697cd1ce67cf31c0a653
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2019-12-29 13:39:19 +0800

    custom post options

--- index.js
+++ index.js
@@ -879,7 +879,7 @@ Thanks for participating! 🥳🥳🥳
         }
       });
       console.log("This is the votedusers " + votedUsers);
-      console.log(votedUsers.includes(voter));
+      console.log(votedUsers.includes(voter.toString()));
       if (votedUsers.includes(voter)) {
         console.log("VOTED PATH");
         bot.sendMessage(voter, "You have already voted!");
