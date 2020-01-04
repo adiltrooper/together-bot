9e02a14f3ca729df601bf9d054752434e78416b2
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2020-01-04 14:02:25 +0800

    fix session code

--- existPollReplyConfig.js
+++ existPollReplyConfig.js
@@ -1,14 +1,14 @@
-module.exports = function(
-  pollOption1,
-  pollOption2,
-  pollOption3,
-  pollOption4,
-  pollVoterLength,
-  option1Result,
-  option2Result,
-  option3Result,
-  option4Result
-) {
+module.exports = async function(pollVoterLength, pollOptions, pollCount) {
+  pollOption1 = pollOptions[0];
+  pollOption2 = pollOptions[1];
+  pollOption3 = pollOptions[2];
+  pollOption4 = pollOptions[3];
+
+  pollCount1 = parseInt(pollCount[0]);
+  pollCount2 = parseInt(pollCount[1]);
+  pollCount3 = parseInt(pollCount[2]);
+  pollCount4 = parseInt(pollCount[3]);
+
   if (pollOption1 && !pollOption2 && !pollOption3 && !pollOption4) {
     return `<b>You have an Existing Poll!</b>
 
@@ -17,6 +17,9 @@ ${pollVoterLength} people have participated!
 <b>Would you like to End it?</b>
       `;
   } else if (pollOption1 && pollOption2 && !pollOption3 && !pollOption4) {
+    totalCount = pollCount1 + pollCount2;
+    option1Result = (pollCount1 / totalCount) * 100;
+    option2Result = (pollCount2 / totalCount) * 100;
     return `<b>You have an Existing Poll!</b>
 
 1️⃣${pollOption1}: <b>${option1Result}%</b>
@@ -27,6 +30,11 @@ ${pollVoterLength} people have participated!
 <b>Would you like to End it?</b>
       `;
   } else if (pollOption1 && pollOption2 && pollOption3 && !pollOption4) {
+    totalCount = pollCount1 + pollCount2 + pollCount3;
+    option1Result = (pollCount1 / totalCount) * 100;
+    option2Result = (pollCount2 / totalCount) * 100;
+    option3Result = (pollCount3 / totalCount) * 100;
+
     return `<b>You have an Existing Poll!</b>
 
 1️⃣${pollOption1}: <b>${option1Result}%</b>
@@ -37,6 +45,11 @@ ${pollVoterLength} people have participated!
 <b>Would you like to End it?</b>
           `;
   } else if (pollOption1 && pollOption2 && pollOption3 && pollOption4) {
+    totalCount = pollCount1 + pollCount2 + pollCount3 + pollCount4;
+    option1Result = (pollCount1 / totalCount) * 100;
+    option2Result = (pollCount2 / totalCount) * 100;
+    option3Result = (pollCount3 / totalCount) * 100;
+    option4Result = (pollCount4 / totalCount) * 100;
     return `<b>You have an Existing Poll!</b>
 
 1️⃣${pollOption1}: <b>${option1Result}%</b>
--- index.js
+++ index.js
@@ -178,8 +178,6 @@ bot.onText(/Poll Post/, async msg => {
     console.log(err.message);
   });
 
-  console.log(pollExists);
-
   if (adminState == "admin1" && pollExists) {
     session.setAdminState("4");
 
@@ -192,93 +190,32 @@ bot.onText(/Poll Post/, async msg => {
         console.log(err.message);
       });
 
-      pollOption1 = pollOptions[0];
-      pollOption2 = pollOptions[1];
-      pollOption3 = pollOptions[2];
-      pollOption4 = pollOptions[3];
-
       const pollCount = await session.getPollCount().catch(err => {
         console.log(err.message);
       });
-      pollCount1 = parseInt(pollCount[0]);
-      pollCount2 = parseInt(pollCount[1]);
-      pollCount3 = parseInt(pollCount[2]);
-      pollCount4 = parseInt(pollCount[3]);
 
       if (pollOption1 && !pollOption2 && !pollOption3 && !pollOption4) {
         bot.sendMessage(
           msg.chat.id,
-          existPollReplyConfig(
-            pollOption1,
-            pollOption2,
-            pollOption3,
-            pollOption4,
-            pollVoterLength
-          ),
+          existPollReplyConfig(pollOptions, pollCount, pollVoterLength),
           existPollReplyMarkup()
         );
       } else if (pollOption1 && pollOption2 && !pollOption3 && !pollOption4) {
-        totalCount = pollCount1 + pollCount2;
-        console.log(totalCount);
-        option1Result = (pollCount1 / totalCount) * 100;
-        option2Result = (pollCount2 / totalCount) * 100;
-
         bot.sendMessage(
           msg.chat.id,
-          existPollReplyConfig(
-            pollOption1,
-            pollOption2,
-            pollOption3,
-            pollOption4,
-            pollVoterLength,
-            option1Result,
-            option2Result
-          ),
+          existPollReplyConfig(pollOptions, pollCount, pollVoterLength),
           existPollReplyMarkup()
         );
       } else if (pollOption1 && pollOption2 && pollOption3 && !pollOption4) {
-        totalCount = pollCount1 + pollCount2 + pollCount3;
-        console.log(pollCount1);
-        console.log(totalCount);
-        option1Result = (pollCount1 / totalCount) * 100;
-        option2Result = (pollCount2 / totalCount) * 100;
-        option3Result = (pollCount3 / totalCount) * 100;
-
         bot.sendMessage(
           msg.chat.id,
-          existPollReplyConfig(
-            pollOption1,
-            pollOption2,
-            pollOption3,
-            pollOption4,
-            pollVoterLength,
-            option1Result,
-            option2Result,
-            option3Result
-          ),
+          existPollReplyConfig(pollOptions, pollCount, pollVoterLength),
           existPollReplyMarkup()
         );
       } else if (pollOption1 && pollOption2 && pollOption3 && pollOption4) {
-        totalCount = pollCount1 + pollCount2 + pollCount3 + pollCount4;
-        console.log(totalCount);
-        option1Result = (pollCount1 / totalCount) * 100;
-        option2Result = (pollCount2 / totalCount) * 100;
-        option3Result = (pollCount3 / totalCount) * 100;
-        option4Result = (pollCount4 / totalCount) * 100;
-
         bot.sendMessage(
           msg.chat.id,
-          existPollReplyConfig(
-            pollOption1,
-            pollOption2,
-            pollOption3,
-            pollOption4,
-            pollVoterLength,
-            option1Result,
-            option2Result,
-            option3Result,
-            option4Result
-          ),
+          existPollReplyConfig(pollOptions, pollCount, pollVoterLength),
           existPollReplyMarkup()
         );
       }
