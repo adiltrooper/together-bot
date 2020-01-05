d2492460a1139d7ace7e5a22a47dc0084fd79602
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2020-01-05 12:45:21 +0800

    fix website null error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { bot, pool } = require("./config/config_bot");
 const { session } = require("./session");
-const { storeNewUser } = require("./storage");
+const { storeNewUser, getSubsCount } = require("./storage");
 
 const keys = require("./config/config_keys/keys");
 const _ = require("lodash/array");
@@ -48,21 +48,7 @@ What can this bot do for you?
       parse_mode: "HTML"
     }
   );
-
   storeNewUser(chat_id, first_name, username, user_type, status);
-
-  // pool.getConnection(function(err, connection) {
-  //   if (err) console.log(err);
-  //   connection.query(
-  //     "INSERT INTO bot_user_db (chat_id, first_name, username, user_type, status) VALUES (?, ?, ?, ?, ?)",
-  //     [chat_id, first_name, username, user_type, status],
-  //     function(err, results, fields) {
-  //       if (err) console.log(err.message);
-  //     }
-  //   );
-  //   connection.release();
-  //   if (err) console.log(err);
-  // });
 });
 
 /////////////// ADMIN CHECK FUNCTION ///////////////////
@@ -125,25 +111,7 @@ bot.onText(/Subscriber Count/, async msg => {
     console.log(err.message);
   });
   if (adminState == "admin1") {
-    pool.getConnection(function(err, connection) {
-      if (err) console.log(err);
-      connection.query(
-        "SELECT COUNT(*) AS subsCount FROM bot_user_db",
-        function(err, results, fields) {
-          if (err) console.log(err.message);
-          console.log(results[0].subsCount);
-          var subsCount = results[0].subsCount;
-
-          bot.sendMessage(
-            msg.chat.id,
-            `TogetherSG now has <b>${subsCount}</b> subsribers!`,
-            { parse_mode: "HTML" }
-          );
-        }
-      );
-      connection.release();
-      if (err) console.log(err);
-    });
+    getSubsCount();
   }
 });
 
@@ -227,32 +195,19 @@ bot.on("callback_query", async callbackQuery => {
       console.log(err.message);
     });
 
-    pool.getConnection(function(err, connection) {
-      connection.query(
-        "INSERT INTO bot_poll (title, created_by, option1, option1_count, option2, option2_count, option3, option3_count, option4, option4_count) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [
-          pollTitle,
-          callbackQuery.from.id,
-          pollOption1,
-          pollCount1,
-          pollOption2,
-          pollCount2,
-          pollOption3,
-          pollCount3,
-          pollOption4,
-          pollCount4
-        ],
-        function(err, results, fields) {
-          if (err) {
-            console.log(err.message);
-          } else {
-            console.log(`${pollTitle} poll has been inserted into database`);
-          }
-        }
-      );
-      connection.release();
-      if (err) console.log(err);
-    });
+    storeCompletePoll(
+      pollTitle,
+      callbackQuery,
+      pollOption1,
+      pollCount1,
+      pollOption2,
+      pollCount2,
+      pollOption3,
+      pollCount3,
+      pollOption4,
+      pollCount4
+    );
+
     session.delPollData();
     session.delPollVoter();
     session.setAdminState("5");
@@ -927,6 +882,17 @@ ${short_desc}
         return (caption = `<b>☀️${activity} @ ${location}☀️</b>
 
 ${short_desc}`);
+      } else if (
+        price !== "null" &&
+        price !== "0" &&
+        poi !== "null" &&
+        website == "null"
+      ) {
+        return (caption = `<b>☀️${activity} @ ${location}☀️</>
+
+${short_desc}
+
+📍: ${poi}`);
       }
     }
 
@@ -1030,6 +996,16 @@ ${short_desc}
 
 ${short_desc}
             `);
+                  } else if (
+                    (price !== null || price !== 0) &&
+                    poi !== null &&
+                    website == null
+                  ) {
+                    return (caption2 = `<b>☀️${activity} @ ${location}☀️</>
+
+  ${short_desc}
+
+  📍: ${poi}`);
                   } else {
                     console.log("FAILUREEEE");
                   }
--- storage.js
+++ storage.js
@@ -20,3 +20,66 @@ exports.storeNewUser = function storeNewUser(
     if (err) console.log(err);
   });
 };
+
+exports.storeCompletePoll = function storeCompletePoll(
+  pollTitle,
+  callbackQuery,
+  pollOption1,
+  pollCount1,
+  pollOption2,
+  pollCount2,
+  pollOption3,
+  pollCount3,
+  pollOption4,
+  pollCount4
+) {
+  pool.getConnection(function(err, connection) {
+    connection.query(
+      "INSERT INTO bot_poll (title, created_by, option1, option1_count, option2, option2_count, option3, option3_count, option4, option4_count) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        pollTitle,
+        callbackQuery.from.id,
+        pollOption1,
+        pollCount1,
+        pollOption2,
+        pollCount2,
+        pollOption3,
+        pollCount3,
+        pollOption4,
+        pollCount4
+      ],
+      function(err, results, fields) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log(`${pollTitle} poll has been inserted into database`);
+        }
+      }
+    );
+    connection.release();
+    if (err) console.log(err);
+  });
+};
+
+exports.getSubsCount = function getSubsCount() {
+  pool.getConnection(function(err, connection) {
+    if (err) console.log(err);
+    connection.query("SELECT COUNT(*) AS subsCount FROM bot_user_db", function(
+      err,
+      results,
+      fields
+    ) {
+      if (err) console.log(err.message);
+      console.log(results[0].subsCount);
+      var subsCount = results[0].subsCount;
+
+      bot.sendMessage(
+        msg.chat.id,
+        `TogetherSG now has <b>${subsCount}</b> subsribers!`,
+        { parse_mode: "HTML" }
+      );
+    });
+    connection.release();
+    if (err) console.log(err);
+  });
+};
