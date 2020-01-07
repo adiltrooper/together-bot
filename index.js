e72975dbfe66c96a0d16256d9421eb060c419bb0
adiltrooper <36306955+adiltrooper@users.noreply.github.com>
2020-01-07 22:05:02 +0800

    address LPUSH null issue

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ bot.onText(/Subscriber Count/, async msg => {
   });
   if (adminState == "admin1") {
     console.log("TRYING");
-    await getSubsCount().catch(err => {
+    let subsCount = getSubsCount().catch(err => {
       console.log(err.message);
     });
     console.log(subsCount + "HELLO");
